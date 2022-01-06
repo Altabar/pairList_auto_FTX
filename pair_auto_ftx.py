d7d07f3dae1f3d965bf7c9d079340defb38f1e45
Altabar <91147328+Altabar@users.noreply.github.com>
2022-01-06 21:40:01 +0100

    Add files via upload

--- pair_auto_ftx.py
+++ pair_auto_ftx.py
@@ -40,22 +40,29 @@ dfi.drop(indexNames , inplace=True)
 dfi['change24h'] = pd.to_numeric(dfi['change24h'])
 dfi['price'] = pd.to_numeric(dfi['price'])
 # exemple plage de prix
-indexNames = dfi[ dfi['price'] < 0.0001].index
+indexNames = dfi[ dfi['price'] < 0.001].index
 dfi.drop(indexNames , inplace=True)
 # exemple pour l'achat (que des valeurs baissières)
 indexNames = dfi[ dfi['change24h'] > 0 ].index
 dfi.drop(indexNames , inplace=True)
-# retrait des BULL
+# retrait divers
 indexNames = dfi[ dfi['name'].str.contains('BULL')].index
 dfi.drop(indexNames , inplace=True)
 indexNames = dfi[ dfi['name'].str.contains('HALF')].index
 dfi.drop(indexNames , inplace=True)
+indexNames = dfi[ dfi['name'].str.contains('HEDGE')].index
+dfi.drop(indexNames , inplace=True)
+indexNames = dfi[ dfi['name'].str.contains('BEAR')].index
+dfi.drop(indexNames , inplace=True)
 indexNames = dfi[ dfi['tokenizedEquity'] == 'True' ].index
 dfi.drop(indexNames , inplace=True)
-print(dfi.info())
+#print(dfi.info())
 # la pairList
 pairList = dfi.name.values.tolist()
 print(pairList)
+print()
+var =  dfi.shape[0]
+print("Nombre de crypto: ", var, "en", df.quote[1])
 
 ftx = SpotFtx(
         apiKey='',
