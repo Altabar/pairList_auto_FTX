70c91ae3e872bb652e5b6f7663c25b7dd1c82f41
Altabar <91147328+Altabar@users.noreply.github.com>
2022-01-06 19:44:58 +0100

    Add files via upload

--- pair_auto_ftx.py
+++ pair_auto_ftx.py
@@ -16,16 +16,9 @@ now = datetime.now()
 print(now.strftime("%d-%m %H:%M:%S"))
 print()
 
-ftx = SpotFtx(
-        apiKey='',
-        secret='',
-        subAccountName=''
-    )
-
 ftx = ccxt.ftx()
 markets = ftx.load_markets()
 df = pd.DataFrame.from_dict(markets, orient='index')
-# quoteCurrency': 'USD' spot enabled active
 indexNames = df[ df['quote'] != 'USD' ].index
 df.drop(indexNames , inplace=True)
 indexNames = df[ df['spot'] == 'False' ].index
@@ -36,27 +29,37 @@ indexNames = df[ df['future'] == 'True' ].index
 df.drop(indexNames , inplace=True)
 indexNames = df[ df['type'] != 'spot' ].index
 df.drop(indexNames , inplace=True)
-#print(df.info())
-#print("------------------iloc df['info']0 17 (df['info']) :")
-#print(df.iloc[0,17])
 pairs = df['info']
 dict = pairs.to_dict()
 dfi = pd.DataFrame.from_dict(dict, orient='index')
-#print(dfi.info())
-#print("------------------iloc dfi 0 20 (dfi['volumeUsd24h']) :")
-#print(dfi.iloc[0,20])
 dfi['volumeUsd24h'] = pd.to_numeric(dfi['volumeUsd24h'])
+indexNames = dfi[ dfi['volumeUsd24h'] < 50000 ].index
+dfi.drop(indexNames , inplace=True)
+indexNames = dfi[ dfi['enabled'] == 'False' ].index
+dfi.drop(indexNames , inplace=True)
+dfi['change24h'] = pd.to_numeric(dfi['change24h'])
 dfi['price'] = pd.to_numeric(dfi['price'])
-#exemple plage de prix
+# exemple plage de prix
 indexNames = dfi[ dfi['price'] < 0.0001].index
 dfi.drop(indexNames , inplace=True)
-#exemple pour l'achat (que des valeurs baissières)
+# exemple pour l'achat (que des valeurs baissières)
 indexNames = dfi[ dfi['change24h'] > 0 ].index
 dfi.drop(indexNames , inplace=True)
-#retrait des BULL
+# retrait des BULL
 indexNames = dfi[ dfi['name'].str.contains('BULL')].index
+dfi.drop(indexNames , inplace=True)
+indexNames = dfi[ dfi['name'].str.contains('HALF')].index
+dfi.drop(indexNames , inplace=True)
+indexNames = dfi[ dfi['tokenizedEquity'] == 'True' ].index
 dfi.drop(indexNames , inplace=True)
 print(dfi.info())
 # la pairList
 pairList = dfi.name.values.tolist()
 print(pairList)
+
+ftx = SpotFtx(
+        apiKey='',
+        secret='',
+        subAccountName=''
+    )
+
