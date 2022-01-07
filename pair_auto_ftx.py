14daaa25eed9f9bd1b127132a35b1b84186e0bdf
Altabar <91147328+Altabar@users.noreply.github.com>
2022-01-07 12:03:35 +0100

    Add files via upload

--- pair_auto_ftx.py
+++ pair_auto_ftx.py
@@ -14,7 +14,6 @@ pd.options.display.max_columns =100
 
 now = datetime.now()
 print(now.strftime("%d-%m %H:%M:%S"))
-print()
 
 ftx = ccxt.ftx()
 markets = ftx.load_markets()
@@ -32,8 +31,11 @@ df.drop(indexNames , inplace=True)
 pairs = df['info']
 dict = pairs.to_dict()
 dfi = pd.DataFrame.from_dict(dict, orient='index')
+dfi['volumeUsd24h'] = pd.to_numeric(dfi['volumeUsd24h'])
 indexNames = dfi[ dfi['enabled'] == 'False' ].index
 dfi.drop(indexNames , inplace=True)
+dfi['change24h'] = pd.to_numeric(dfi['change24h'])
+dfi['price'] = pd.to_numeric(dfi['price'])
 # retrait divers
 indexNames = dfi[ dfi['name'].str.contains('BULL')].index
 dfi.drop(indexNames , inplace=True)
@@ -48,10 +50,8 @@ dfi.drop(indexNames , inplace=True)
 indexNames = dfi[ dfi['tokenizedEquity'] == 'True' ].index
 dfi.drop(indexNames , inplace=True)
 #print(dfi.info())
-# la pairList
 pairList = dfi.name.values.tolist()
 print(pairList)
-print()
 var =  dfi.shape[0]
 print("Nombre de cryptos: ", var, "en", df.quote[1])
 
